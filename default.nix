7863d6dd35ad65ec10bd748498c4c22de20cc523
FaustXVI <xavier.detant@gmail.com>
2021-09-14 17:49:00 +0200

    Update rules to be COVID complient

--- default.nix
+++ default.nix
@@ -1,19 +1,11 @@
+with import (fetchTarball https://github.com/NixOS/nixpkgs/archive/21.05.tar.gz) {};
 let
-  _pkgs = import <nixpkgs> {};
-  nixpkgs = _pkgs.fetchFromGitHub { owner = "NixOS";
-                                         repo = "nixpkgs-channels";
-                                         rev = "39cd40f7bea40116ecb756d46a687bfd0d2e550e";
-                                         sha256 = "0kpx4h9p1lhjbn1gsil111swa62hmjs9g93xmsavfiki910s73sh";
-                                       };
-in
-with import nixpkgs {};
-let
-    pgf = import ./nix/pgf.nix nixpkgs;
+    pgf = import ./nix/pgf.nix <nixpkgs>;
 in
 stdenv.mkDerivation {
         name = "latex-sandbox";
         TEXMFHOME="${pgf}/${pgf.TEXMFHOME}"; 
         buildInputs = [
-            tetex
+            texlive.combined.scheme-full
         ];
     }
--- nix/pgf.nix
+++ nix/pgf.nix
@@ -9,7 +9,7 @@ stdenv.mkDerivation {
         TEXMFHOME="texmf";
         src = fetchurl {
             url = "http://mirrors.ctan.org/graphics/pgf/base.zip";
-            sha256 = "0dsc6ybl2b7n1w8zgr0bcwdmzvmm86qpy7vla88lh2zrcgyii913";
+            sha256 = "0ycdngi71vmp1n75xzpi7bd1d7lr5r7id21kx3l0b16ziyjqcvjg";
         };
         buildPhase = ''
             mkdir -p $TEXMFHOME
